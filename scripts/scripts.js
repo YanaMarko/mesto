f4356f1fde79ade3f0bc0680f428c78abcbf94eb
Yana Markova <yana.markova.98@internet.ru>
2021-11-03 02:45:29 +0300

    add: js

---
+++ scripts/scripts.js
@@ -0,0 +1,38 @@
+// обратимся к блоку
+const popupElement = document.querySelector('.pop-up');
+// обратимся к вызывающей поп-ап кнопке
+const editButtonElement = document.querySelector('.profile__edit-btn');
+// обратимся к закрывающей поп-ап кнопке
+const closeButtonElement = document.querySelector('.pop-up__close-btn');
+// пропишем условие вызова поп-апа
+editButtonElement.addEventListener('click', openPopup);
+// пропишем условие закрытия поп-апа
+closeButtonElement.addEventListener('click', closePopup);
+
+// вызовем поп-ап
+function openPopup() {
+    popupElement.classList.remove('pop-up_opened');
+}
+// закроем поп-ап
+function closePopup() {
+    popupElement.classList.add('pop-up_opened');
+}
+
+
+
+// обратимся к белому лайку
+const like = document.querySelector('.content__like');
+// обратимся к чёрному лайку
+const activeLike = document.querySelector('.content__like_visited');
+// пропишем условие отметки лайка
+like.addEventListener('click', iLike);
+// пропишем условие отметы лайка
+activeLike.addEventListener('click', iDontLike);
+// отметим лайк
+function iLike() {
+    like.classList.add('content__like_visited');
+}
+// отменим лайк
+function iDontLike() {
+    like.classList.remove('content__like_visited');
+}
