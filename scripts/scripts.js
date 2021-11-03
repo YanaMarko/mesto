a9591c7632dc223f63df6ed1a96e19635e18c277
Yana Markova <yana.markova.98@internet.ru>
2021-11-03 17:39:36 +0300

    fix: js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,56 +1,53 @@
-// обратимся к блоку
+// ПОП-АП
+
 const popupElement = document.querySelector('.pop-up');
-// обратимся к вызывающей поп-ап кнопке
 const editButtonElement = document.querySelector('.profile__edit-btn');
-// обратимся к закрывающей поп-ап кнопке
 const closeButtonElement = document.querySelector('.pop-up__close-btn');
-// пропишем условие вызова поп-апа
+
 editButtonElement.addEventListener('click', openPopup);
-// пропишем условие закрытия поп-апа
 closeButtonElement.addEventListener('click', closePopup);
 
-// вызовем поп-ап
 function openPopup() {
     popupElement.classList.remove('pop-up_opened');
 }
-// закроем поп-ап
+
 function closePopup() {
     popupElement.classList.add('pop-up_opened');
 }
 
+// ФОРМА
 
+const formElement = document.querySelector('.pop-up-form');
+const userName = document.querySelector('.profile__title');
+const userDescription = document.querySelector('.profile__subtitle');
+const formName = document.querySelector('.pop-up-form__name');
+const formDescription = document.querySelector('.pop-up-form__description');
+const saveButton = document.querySelector('.pop-up__save');
 
-// обратимся к белому лайку
+function changeName() {
+    const newName = formName.value;
+    userName.textContent = newName;
+}
+saveButton.addEventListener('click', changeName);
+
+function changeDescription() {
+    const newDescription = formDescription.value;
+    userDescription.textContent = newDescription;
+}
+saveButton.addEventListener('click', changeDescription);
+
+// ЛАЙКИ
+
 const like = document.querySelector('.content__like');
-// обратимся к чёрному лайку
 const activeLike = document.querySelector('.content__like_visited');
-// пропишем условие отметки лайка
+
 like.addEventListener('click', iLike);
-// пропишем условие отметы лайка
 activeLike.addEventListener('click', iDontLike);
-// отметим лайк
+
 function iLike() {
     like.classList.add('content__like_visited');
 }
-// отменим лайк
+
 function iDontLike() {
     like.classList.remove('content__like_visited');
 }
-
-// // обратимся к блоку 
-// const formElement = document.querySelector('.pop-up-form');
-// // обратимся к имени пользователя
-// const userName = userName.getAttribute('value');
-// // обратимся к описанию профиля
-// const userdescription = userdescription.getAttribute('value');
-// // изменение имени пользователя в профиле
-
-// // измение описания профиля
-
-
-// // сохраним изменения
-
-
-
-// // onchange = "onchangeName()"
-// // onchange = "onchangeDescription()"
