166bd7d8e5387b1069bb493b2503f935b99e669c
Yana Markova <yana.markova.98@internet.ru>
2021-11-06 19:31:30 +0300

    fix: html, css, js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,27 +2,25 @@ const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-btn');
 const closeButton = document.querySelector('.popup__close-btn');
 const form = document.querySelector('.popup-form');
-const formName = document.querySelector('.popup-form__user_name');
-const formDescription = document.querySelector('.popup-form__user_description');
+const formName = document.querySelector('.popup-form__user_type_name');
+const formDescription = document.querySelector('.popup-form__user_type_description');
 const userName = document.querySelector('.profile__title');
 const userDescription = document.querySelector('.profile__subtitle');
 
 function openPopup() {
-    popup.classList.remove('popup_opened');
+    popup.classList.add('popup_opened');
 }
 
 function closePopup() {
-    popup.classList.add('popup_opened');
+    popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
-    let formName = document.querySelector('.popup-form__user_name').value;
-    userName.textContent = formName;
+    userName.textContent = formName.value
+    userDescription.textContent = formDescription.value;
 
-    let formDescription = document.querySelector('.popup-form__user_description').value;
-    userDescription.textContent = formDescription;
     closePopup();
 }
 form.addEventListener('submit', formSubmitHandler);
