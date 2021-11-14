b21546f5f8d725e5891980d40f40d5709d65973a
Yana Markova <yana.markova.98@internet.ru>
2021-11-14 23:46:30 +0300

    add: second popupp; fix: css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,6 +7,15 @@ const formDescription = document.querySelector('.popup-form__user_type_descripti
 const userName = document.querySelector('.profile__title');
 const userDescription = document.querySelector('.profile__subtitle');
 
+const popupPlace = document.querySelector('.popup-place');
+const openPlaceButton = document.querySelector('.profile__add-photo-btn');
+const closePlaceButton = document.querySelector('.popup-place__close-btn');
+const formPlace = document.querySelector('.popup-form-place');
+const formPlaceName = document.querySelector('.popup-form__place_type_name');
+const formPlacePhoto = document.querySelector('.popup-form__place_type_photo');
+const placeName = document.querySelector('.content__title');
+const placePhoto = document.querySelector('.content__img');
+
 function openPopup() {
     formName.value = userName.textContent;
     formDescription.value = userDescription.textContent;
@@ -19,12 +28,31 @@ function closePopup() {
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-
     userName.textContent = formName.value;
     userDescription.textContent = formDescription.value;
-
     closePopup();
 }
 form.addEventListener('submit', formSubmitHandler);
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
+
+
+
+function openPlacePopup() {
+    formPlaceName.value = placeName.textContent;
+    formPlacePhoto.value = placePhoto.textContent;
+    popupPlace.classList.add('popup_opened');
+}
+
+function closePlacePopup() {
+    popupPlace.classList.remove('popup_opened');
+}
+openPlaceButton.addEventListener('click', openPlacePopup);
+closePlaceButton.addEventListener('click', closePlacePopup);
+
+function formPlaceSubmitHandler(evt) {
+    evt.preventDefault();
+    placeName.textContent = formPlaceName.value;
+    placePhoto.textContent = formPlacePhoto.value;//должно менять src на ссылку
+    closePlacePopup();
+}
