68b7e29f0b3007a641eee790d68f2786f33a8618
Yana Markova <yana.markova.98@internet.ru>
2021-11-22 22:32:08 +0300

    fix: js[C

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,27 @@
+//не могу продвинуться дальше
+
 const popup = document.querySelector('.popup');
 const formName = document.querySelector('.popup-form__user_type_name');
 const formDescription = document.querySelector('.popup-form__user_type_description');
 const userName = document.querySelector('.profile__title');
 const userDescription = document.querySelector('.profile__subtitle');
-const popupTaskFormName = document.querySelector('.popup-form__task_type_name');
-const popupTaskFormPhoto = document.querySelector('.popup-form__task_type_link');
-const cardName = document.querySelector('.card__title');
-const cardPhoto = document.querySelector('.card__img');
+let popupTaskFormName = document.querySelector('.popup-form__task_type_name');
+let popupTaskFormPhoto = document.querySelector('.popup-form__task_type_link');
+let cardName = document.querySelector('.card__title');
+let cardPhoto = document.querySelector('.card__img');
 const popupTask = document.querySelector('.popup-task');
-const cardContainer = document.querySelector('.template');
+let template = document.querySelector('.template');
+const task = document.querySelector('.task');
+const like = document.querySelector('.card__like');
+const savePopupTask = document.querySelector('.popup-save-task');
+const openTaskButton = document.querySelector('.profile__add-photo-btn');
+const form = document.querySelector('.popup-form');
+const editButton = document.querySelector('.profile__edit-btn');
+const closeButton = document.querySelector('.popup__close-btn');
+const closeTaskButton = document.querySelector('.popup-task__close-btn');
+const popupTaskForm = document.querySelector('.task-form');
+const deleteCardButton = document.querySelector('.card__delete');
+const cards = document.querySelector('.cards');
 
 const initialCards = [
     {
@@ -42,71 +55,86 @@ function openPopup() {
     formDescription.value = userDescription.textContent;
     popup.classList.add('popup_opened');
 }
-const editButton = document.querySelector('.profile__edit-btn').addEventListener('click', openPopup);
+editButton.addEventListener('click', openPopup);
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
-const closeButton = document.querySelector('.popup__close-btn').addEventListener('click', closePopup);
+closeButton.addEventListener('click', closePopup);
 
-function formSubmitHandler(evt) {
+function submitFormHandler(evt) {
     evt.preventDefault();
     userName.textContent = formName.value;
     userDescription.textContent = formDescription.value;
     closePopup();
 }
-const form = document.querySelector('.popup-form').addEventListener('submit', formSubmitHandler);
+form.addEventListener('submit', submitFormHandler);
 
 function openTaskPopup() {
-    // popupTaskFormName.value = cardName.textContent;
-    // popupTaskFormPhoto.value = cardPhoto.src;
     popupTask.classList.add('popup_opened');
 }
-const openTaskButton = document.querySelector('.profile__add-photo-btn').addEventListener('click', openTaskPopup);
+openTaskButton.addEventListener('click', openTaskPopup);
 
 function closeTaskPopup() {
     popupTask.classList.remove('popup_opened');
 }
-const closeTaskButton = document.querySelector('.popup-task__close-btn').addEventListener('click', closeTaskPopup);
+closeTaskButton.addEventListener('click', closeTaskPopup);
+//клонирование карточки
+const createTaskDomNode = (item) => {
+    const taskTemplate = template.content.querySelector('.task').cloneNode(true);
+    taskTemplate.querySelector('.card__title').textContent = item.title;
+    taskTemplate.querySelector('.card__img').src = item.link;
+    return taskTemplate;
+}
+const result = initialCards.map((item) => {
+    return createTaskDomNode(item);
+});
 
-function formSubmitHandlerTask(evt) {
-    evt.preventDefault();
-    cardName.textContent = popupTaskFormName.value;
-    cardPhoto.src = popupTaskFormPhoto.value;
+savePopupTask.onclick = function () {
+    addNewCard()
     closeTaskPopup()
 }
-const popupTaskForm = document.querySelector('.task-form').addEventListener('click', formSubmitHandlerTask);
-
-
-
+//изменение данных через форму
+function submitFormHandlerTask(evt) {
+    evt.preventDefault();
+    const cardNameValue = cardName.title;
+    const cardPhotoValue = cardPhoto.link;
+    const cardString = createTaskDomNode({ title: cardNameValue, link: cardPhotoValue });
+    task.preppend(cardString);
+    cardName.value = '';
+    cardPhoto.value = '';
+};
+popupTaskForm.addEventListener('submit', submitFormHandlerTask);
+task.append(...result)
+//создание карточки
 function createNewCard(item) {
-    const cardTemplate = document.querySelector('#template').content;
-    cardContainer.cloneNode(true);
-    cardName.textContent = item.name;
+    const card = template.content.querySelector('.task');
+    template.cloneNode(true);
+    cardName = item.name;
     cardPhoto.src = item.link;
     cardPhoto.alt = item.name;
-    return cardContainer;
 }
-
 initialCards.forEach(function (item) {
     addNewCard(item);
 });
-
-function deleteCard(evt) {
-    evt.target.closest('.card__item').remove();
-}
-const deleteCardButton = document.querySelector('.card__delete').addEventListener('click', deleteCard);
-
-function likeCard() {
-    like.classList.toggle('.card__like_type_active');
-}
-const like = document.querySelector('.card__like').addEventListener('click', likeCard);
-
+//добавление карточки
 function addNewCard(item) {
-    const cardContainer = createNewCard(item);
-    document.querySelector('.cards').prepend(cardContainer);
-    document.querySelector('.card__like').addEventListener('click', likeCard);
-    document.querySelector('.card__delete').addEventListener('click', () => deleteCard(cardContainer));
-    closeTaskPopup()
+    template = submitFormHandlerTask(item);
+    cards.prepend(template);
+    like.addEventListener('click', likeCard);
+    deleteCardButton.addEventListener('click', () => deleteCard(template));
+    template.insertAdjacentElement('beforebegin', cardName);
+    template.insertAdjacentElement('beforebegin', cardPhoto);
+    template.append(...result);
 }
-const savePopupTask = document.querySelector('.popup-save-task').addEventListener('click', addNewCard);
+savePopupTask.addEventListener('click', addNewCard);
+
+// function deleteCard(evt) {
+//     evt.target.classList.closest('card__item').add('.card__delete');
+// }
+// deleteCardButton.addEventListener('click', deleteCard);
+
+// function likeCard() {
+//     evt.target.classList.toggle('content__like_type_active');
+// }
+// like.addEventListener('click', likeCard)
